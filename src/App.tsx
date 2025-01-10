18c30974bd116ee34b7c04ca64f00eaa9f7d0860
l69luiz <l69luiz@gmail.com>
2025-01-10 16:15:57 -0300

    Menu Lateral

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { AppThemeProvider } from './shared/contexts';
-
-
+import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
   <AppThemeProvider>
     <BrowserRouter>
-      <AppRoutes />
+      <MenuLateral>
+       <AppRoutes />
+      </MenuLateral>
+      
     </BrowserRouter>
     </AppThemeProvider>    
   );
---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Avatar, Box, Divider, Drawer, Icon, List, ListItem, ListItemButton, ListItemIcon, ListItemText, useTheme } from "@mui/material";
+//import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+
+interface MenuLateralProviderProps {
+    children: React.ReactNode;
+}
+
+
+export const MenuLateral: React.FC<MenuLateralProviderProps> = ({children}) => {
+const theme = useTheme();
+
+return (
+    <>
+    <Drawer variant ='permanent'>
+        <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column">
+        <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
+        <Avatar
+         sx={{height: theme.spacing(12), width: theme.spacing(12)}}
+         src="https://yt3.ggpht.com/grfYgQadT8iNg9WPb-jkrKB-9224y_DBDXAOtV4Yt7cyQmtR47J_453uveQOTDsp_dRSH851TMM=s108-c-k-c0x00ffffff-no-rj" 
+        />
+        </Box> 
+        <Divider/>
+        <Box flex={1}>
+        <nav aria-label="Menu principal">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Icon>home</Icon>
+              </ListItemIcon>
+              <ListItemText primary="Página inicial" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <Icon>directions_car</Icon>
+                
+              </ListItemIcon>
+              <ListItemText primary="Veiculos" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+        </Box>   
+
+        Teste
+        </Box>
+    </Drawer>
+    <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+    </Box>
+    </>
+
+);
+}
